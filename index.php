83a4bccf82c88940c4c3d1a6b326f7b9f1d59e99
theblakelalonde <78009242+theblakelalonde@users.noreply.github.com>
2021-02-11 19:54:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
   if (empty($email)){
     $error .= '<p class="error">Please enter your password.</p>';
   }
-  if (empty($error)) {
+  // validate if password is empty
+  if (empty($password))
+  {
+    $error .='<p class="error">Please enter your password.</p>';
+  } 
+  if (empty($error)) 
+  {
+    if($query = $db->prepare("SELECT * FROM users WHERE email = ?")) 
+    {
+      $query->bind_param('s', $email);
+      $query->execute();
+      $row = $query->fetch();
+      if ($row) 
+      {
+        if (password_verify{$password, $row['password'])) 
+        {
+          $_SESSION["userid"] = $row['id'];
+          $_SESSION["user"] = $row;
+          // Redirect the user to welcome page
+          header("location: welcome.php");
+          exit;
+        } 
+        else 
+        {
+          $error .= '<p class="error">The password is not valid.</p>';
+        }
+        } else 
+        {
+          $error .= '<p class="error">No User exists with that email address.</p>';
+        }
+      }
+      $query->close();
+    }
+    // Close connection
+    mysquli_close($db);
+  }    
+?>
 
-    
 
 <!DOCTYPE html>
 <html lang="en">
