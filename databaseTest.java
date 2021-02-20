d24e2451be53299e750851bc90c0e9508ef543ab
theblakelalonde <78009242+theblakelalonde@users.noreply.github.com>
2021-02-19 20:35:21 -0600

    Update databaseTest.java

--- databaseTest.java
+++ databaseTest.java
@@ -1,38 +1,47 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class CsvWriter 
+public class insertUser
 {
-  public static void main(String[] args) 
+	public static void main(String[] args) 
+	{
+		NewAccount obj1 = new NewAccount();
+		
+		String email = obj1.getemail();
+		String username = obj1.getusername();
+		String password = obj1.getpassword();
+		String role = obj1.getrole();
+		File database = new File("database.csv");
+
+		insertToDatabase(database, email, username, password, role);
+	}
+  
+    
+  
+  public static void insertToDatabase(File file, String email, String username, String password, String role)
   {
+	  try (FileWriter writer = new FileWriter(file, true))
+	  {
 
-    try (PrintWriter writer = new PrintWriter(new File("database.csv"))) 
-    {
+	      StringBuilder sb = new StringBuilder();
+	      sb.append(email);
+	      sb.append(',');
+	      sb.append(username);
+	      sb.append(',');
+	      sb.append(password);
+	      sb.append(',');
+	      sb.append(role);
+	      sb.append('\n');
 
-      StringBuilder sb = new StringBuilder();
-      sb.append("Username");
-      sb.append(',');
-      sb.append("Password");
-      sb.append('\n');
+	      writer.write(sb.toString());
 
-      sb.append("theblakelalonde");
-      sb.append(',');
-      sb.append("Present_2021");
-      sb.append('\n');
-      
-      sb.append("trevor");
-      sb.append(',');
-      sb.append("huval");
-      sb.append('\n');
-
-      writer.write(sb.toString());
-
-      System.out.println("done!");
-    } 
-    catch (FileNotFoundException e) 
-    {
-      System.out.println(e.getMessage());
-    }
+	      System.out.println("done!");
+	  } 
+	  catch (IOException e) 
+	  {
+		  // TODO Auto-generated catch block
+		  e.printStackTrace();
+	  } 
   }
 }
