375e44ee8a94ed49dc43212b36c517675f528572
Bryan Tran <68923389+bryantran21@users.noreply.github.com>
2021-03-25 04:08:01 -0500

    Add files via upload

---
+++ ClassGui.java
@@ -0,0 +1,162 @@
+package main;
+
+/**
+ * @author Bryan Tran
+ * @since 3/22/21
+ * @about adding/viewing classes for each student
+ */
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.stream.Stream;
+import javax.swing.JTextField;
+import main.DOMmodifyXML;
+import main.DOMreadXML;
+import static main.DOMmodifyXML.addClass;
+
+public class ClassGui implements ActionListener {
+
+    private JPanel panel;
+    private JFrame frame;
+    private JButton selectButton;
+    private JLabel titleLabel;
+    private JLabel tutorLabel;
+    private JLabel submittedLabel;
+    private JPanel scrollPanel;
+    private JPanel tutorPanel;
+    private JLabel errorLabel;
+    private JList list;
+    private JButton returnButton;
+    private JLabel newLabel;
+    private JScrollPane scrollPane;
+    private JButton submitButton;
+    private JFrame smallFrame;
+    private JPanel smallPanel;
+    private JButton homeButton;
+    private JButton logoutButton;
+    private JTextField addText;
+    private JButton addButton;
+    private JLabel nameLabel;
+    private JComboBox gradeBox;
+    private JLabel courseLabel;
+    private JLabel gradeLabel;
+    private JLabel exampleLabel;
+
+    private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+    private User currentUser;
+
+    public ClassGui(User passedUser) {
+
+        currentUser = passedUser;
+
+        frame = new JFrame();
+        frame.setBounds(0, 0, 800, 600);
+        frame.setLocationRelativeTo(null);
+//        scrollPanel = new JPanel();		// Right panel that hosts objects
+//        scrollPane = new JScrollPane();	// Allows scrolling through list of tutors
+//        submittedLabel = new JLabel(); 	// Label that shows up after rating is submitted
+        panel = new JPanel();
+        panel.setBackground(Color.decode("#36393f"));
+
+        // LEFT PANEL (panel that holds the list of available tutors and the selection button)
+//        scrollPane.setViewportView(list);	// Pane that allows scrolling through the tutors
+//        scrollPane.setBounds(500, 30, 200, 430);
+//        list.setLayoutOrientation(JList.VERTICAL);
+//        errorLabel = new JLabel("");
+//        errorLabel.setBounds(0, 465, 400, 35);
+//        errorLabel.setHorizontalAlignment(JLabel.CENTER);
+//        errorLabel.setFont(new Font(null, Font.CENTER_BASELINE, 12));
+//        errorLabel.setForeground(Color.RED);
+//        scrollPanel.add(errorLabel);
+        String[] grades = {"A", "B", "C", "D", "F"};
+        gradeBox = new JComboBox(grades);
+        gradeBox.setBounds(150, 150, 125, 30);
+        panel.add(gradeBox);
+
+        addText = new JTextField();
+        addText.setBounds(150, 100, 125, 30);
+        panel.add(addText);
+
+        addButton = new JButton("Add Class");
+        addButton.setBounds(150, 200, 125, 30);
+        addButton.setBackground(Color.decode("#7289da"));
+        addButton.setForeground(Color.decode("#dcddde"));
+        panel.add(addButton);
+
+        courseLabel = new JLabel("Enter course: ");
+        courseLabel.setForeground(Color.decode("#dcddde"));
+        courseLabel.setFont(new Font(null, Font.CENTER_BASELINE, 18));
+        courseLabel.setBounds(10, 100, 125, 25);
+        panel.add(courseLabel);
+
+        gradeLabel = new JLabel("Enter grade: ");
+        gradeLabel.setForeground(Color.decode("#dcddde"));
+        gradeLabel.setFont(new Font(null, Font.CENTER_BASELINE, 18));
+        gradeLabel.setBounds(10, 150, 125, 25);
+        panel.add(gradeLabel);
+
+        exampleLabel = new JLabel("(Ex. CSC 3380)");
+        exampleLabel.setForeground(Color.decode("#dcddde"));
+        exampleLabel.setFont(new Font(null, Font.PLAIN, 10));
+        exampleLabel.setBounds(10, 120, 120, 25);
+        panel.add(exampleLabel);
+//
+//        scrollPanel.setLayout(null);	// Entire panel that hosts list of tutors, scroll pane, and button
+//        scrollPanel.add(scrollPane);
+
+        homeButton = new JButton("Home");
+        homeButton.setBounds(600, 10, 80, 30);
+        homeButton.setBackground(Color.decode("#7289da"));
+        homeButton.setForeground(Color.decode("#dcddde"));
+        homeButton.setFocusable(false);
+        homeButton.addActionListener(this);
+        panel.add(homeButton);
+
+        logoutButton = new JButton("Logout");
+        logoutButton.setBounds(690, 10, 80, 30);
+        logoutButton.setBackground(Color.decode("#7289da"));
+        logoutButton.setForeground(Color.decode("#dcddde"));
+        logoutButton.setFocusable(false);
+        logoutButton.addActionListener(this);
+        panel.add(logoutButton);
+
+        frame.setVisible(true);
+        frame.add(panel);
+        panel.setLayout(null);
+        frame.setTitle("Manage Classes");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == addButton) {
+            addClass(currentUser.getEmail(), addText.getText(), gradeBox.getSelectedItem().toString());
+        }
+        if (e.getSource() == homeButton) {
+            HomePage home = new HomePage(currentUser);
+            frame.dispose();
+        }
+        if (e.getSource() == logoutButton) {
+            LoginGui login = new LoginGui();
+            frame.dispose();
+        }
+    }
+}
--- HomePage.java
+++ HomePage.java
@@ -4,7 +4,6 @@ package main;
  *
  * @author Bryan
  */
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -22,8 +21,8 @@ import main.postMeetingReview;
 import main.Search;
 import main.SearchGui;
 
-public class HomePage implements ActionListener
-{  
+public class HomePage implements ActionListener {
+
     private static JPanel panel;
     private static JFrame frame;
     private static JLabel titleLabel;
@@ -36,98 +35,126 @@ public class HomePage implements ActionListener
     private JLabel upcomingLabel;
     private static JButton logoutBtn;
     private User currentUser;
-	
-    String upcomingString[] = {"Monday, March 22   -   csc3380 @ 5:00p","Tuesday, March 23   -   csc3501 @ 9:30a"};
+    private static JButton classButton;
+    private static JButton scheduleButton;
 
-    
-    HomePage(User passedUser){
-    	currentUser = passedUser;
-    	
-    	
+    String upcomingString[] = {"Monday, March 22   -   csc3380 @ 5:00p", "Tuesday, March 23   -   csc3501 @ 9:30a"};
+
+    HomePage(User passedUser) {
+        currentUser = passedUser;
+
         panel = new JPanel();
         frame = new JFrame();
-        frame.setBounds(600,250,800,600);
+        frame.setBounds(600, 250, 800, 600);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
         panel.setBackground(Color.decode("#36393f"));
-        
+
         panel.setLayout(null);
-        
+
         titleLabel = new JLabel("Welcome back, " + currentUser.getFirstName());	// Add user's name
-        titleLabel.setBounds(0,100,800,100);
-        titleLabel.setFont(new Font(null, Font.CENTER_BASELINE,40));
+        titleLabel.setBounds(0, 100, 800, 100);
+        titleLabel.setFont(new Font(null, Font.CENTER_BASELINE, 40));
         titleLabel.setForeground(Color.decode("#dcddde"));
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
         panel.add(titleLabel);
-                
-        searchButton = new JButton("Search For Tutors");
-        searchButton.setBounds(250, 225, 300, 40);
-        searchButton.setBackground(Color.decode("#7289da"));
-        searchButton.setForeground(Color.decode("#dcddde"));
-        searchButton.setFocusable(false);
-        searchButton.addActionListener(this);
-        panel.add(searchButton);
-        
-        reviewButton = new JButton("Post-Meeting Review");
-        reviewButton.setBounds(250, 275, 300, 40);
-        reviewButton.setBackground(Color.decode("#7289da"));
-        reviewButton.setForeground(Color.decode("#dcddde"));
-        reviewButton.setFocusable(false);
-        reviewButton.addActionListener(this);
-        panel.add(reviewButton);
-                 
+
         success = new JLabel("");
         success.setBounds(10, 150, 300, 25);
         panel.add(success);
-        
+
         upcoming = new JList(upcomingString);
         upcoming.setForeground(Color.decode("#23272a"));
         upcoming.setBackground(Color.decode("#99aab5"));
-        
+
         scrollPane = new JScrollPane(upcoming);
         scrollPane.setViewportView(upcoming);
-        scrollPane.setBounds(250,375,300,150);
+        scrollPane.setBounds(250, 375, 300, 150);
         upcoming.setLayoutOrientation(JList.VERTICAL);
         panel.add(scrollPane);
-        
+
         upcomingLabel = new JLabel("Upcoming Meetings");
-        upcomingLabel.setBounds(250,355,300,25);
-        upcomingLabel.setFont(new Font(null,Font.PLAIN,10));
+        upcomingLabel.setBounds(250, 355, 300, 25);
+        upcomingLabel.setFont(new Font(null, Font.PLAIN, 10));
         upcomingLabel.setForeground(Color.decode("#dcddde"));
         panel.add(upcomingLabel);
-        
-	logoutBtn = new JButton("Logout");
-        logoutBtn.setBounds(690,10,80,30);
+
+        logoutBtn = new JButton("Logout");
+        logoutBtn.setBounds(690, 10, 80, 30);
         logoutBtn.setBackground(Color.decode("#7289da"));
         logoutBtn.setForeground(Color.decode("#dcddde"));
         logoutBtn.setFocusable(false);
         logoutBtn.addActionListener(this);
         panel.add(logoutBtn);
-        
+
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setTitle("Goober - Homepage");
 
+        if (currentUser.getRole().equals("Tutor")) {
+            classButton = new JButton("Add/View Classes");
+            classButton.setBounds(250, 225, 300, 40);
+            classButton.setBackground(Color.decode("#7289da"));
+            classButton.setForeground(Color.decode("#dcddde"));
+            classButton.setFocusable(false);
+            classButton.addActionListener(this);
+            panel.add(classButton);
+
+            scheduleButton = new JButton("Manage Schedule");
+            scheduleButton.setBounds(250, 275, 300, 40);
+            scheduleButton.setBackground(Color.decode("#7289da"));
+            scheduleButton.setForeground(Color.decode("#dcddde"));
+            scheduleButton.setFocusable(false);
+            scheduleButton.addActionListener(this);
+            panel.add(scheduleButton);
+
+        } else {
+            searchButton = new JButton("Search For Tutors");
+            searchButton.setBounds(250, 225, 300, 40);
+            searchButton.setBackground(Color.decode("#7289da"));
+            searchButton.setForeground(Color.decode("#dcddde"));
+            searchButton.setFocusable(false);
+            searchButton.addActionListener(this);
+            panel.add(searchButton);
+
+            reviewButton = new JButton("Post-Meeting Review");
+            reviewButton.setBounds(250, 275, 300, 40);
+            reviewButton.setBackground(Color.decode("#7289da"));
+            reviewButton.setForeground(Color.decode("#dcddde"));
+            reviewButton.setFocusable(false);
+            reviewButton.addActionListener(this);
+            panel.add(reviewButton);
+        }
+
     }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == searchButton) // Sends the user to a window to search for tutors
-		{
-			SearchGui search = new SearchGui(currentUser);
-			frame.dispose();
-		}
-		if (e.getSource() == reviewButton) // Sends the user to a page to rate a previous tutor
-		{
-			postMeetingReview pmr = new postMeetingReview(currentUser);
-			frame.dispose();
-		}
-		if (e.getSource() == logoutBtn) // Logs the user out of the application
-                {
-                    LoginGui login = new LoginGui();
-                    frame.dispose();
-                }
-		
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == searchButton) // Sends the user to a window to search for tutors
+        {
+            SearchGui search = new SearchGui(currentUser);
+            frame.dispose();
+        }
+        if (e.getSource() == reviewButton) // Sends the user to a page to rate a previous tutor
+        {
+            postMeetingReview pmr = new postMeetingReview(currentUser);
+            frame.dispose();
+        }
+        if (e.getSource() == logoutBtn) // Logs the user out of the application
+        {
+            LoginGui login = new LoginGui();
+            frame.dispose();
+        }
+        if (e.getSource() == classButton)
+        {
+            ClassGui clgui = new ClassGui(currentUser);
+            frame.dispose();
+        }
+        if(e.getSource() == scheduleButton)
+        {
+            Schedule sch = new Schedule();
+            frame.dispose();
+        }
+    }
 }
