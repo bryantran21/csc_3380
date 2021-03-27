3f676a6d6cc2c34ca60964a7d9767f4914c9136a
Anthony Duong <anthonyduong17@gmail.com>
2021-03-27 15:06:16 -0500

    added delete course btn

--- ClassGui.java
+++ ClassGui.java
@@ -44,44 +44,30 @@ public class ClassGui implements ActionListener {
 
     private JPanel panel;
     private JFrame frame;
-    private JButton selectButton;
     private JLabel titleLabel;
-    private JLabel tutorLabel;
-    private JLabel submittedLabel;
-    private JPanel scrollPanel;
-    private JPanel tutorPanel;
     private JLabel errorLabel;
-    private JList list;
-    private JButton returnButton;
-    private JLabel newLabel;
     private JScrollPane scrollPane;
-    private JButton submitButton;
-    private JFrame smallFrame;
-    private JPanel smallPanel;
     private JButton homeButton;
     private JButton logoutButton;
     private JTextField addText;
     private JButton addButton;
-    private JLabel nameLabel;
+    private JButton deleteBtn;
     private JComboBox gradeBox;
     private JLabel courseLabel;
     private JLabel gradeLabel;
     private JLabel exampleLabel;
-    private JLabel successLabel;
-    private JButton successButton;
     private JFrame confirmFrame;
     private JPanel confirmPanel;
     private JLabel confirmLabel1;
-    private JLabel confirmLabel2;
     private JButton confirmBtn;
     private JButton declineBtn;
+    private JButton confirmBtn2;
     private JFrame successFrame;
     private JLabel scrollPaneLabel;
     private JPanel successPanel;
     private JLabel successLabel1;
-    private JLabel successLabel2;
 
-    private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+//    private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
     private User currentUser;
     private JList courseList;
 
@@ -93,52 +79,51 @@ public class ClassGui implements ActionListener {
         frame.setBounds(0, 0, 800, 600);
         frame.setLocationRelativeTo(null);
 //        scrollPanel = new JPanel();		// Right panel that hosts objects
-       
+
 //        submittedLabel = new JLabel(); 	// Label that shows up after rating is submitted
         panel = new JPanel();
         panel.setBackground(Color.decode("#36393f"));
-        
+
         String emptyArray[] = {""};
-        
+
         courseList = new JList(emptyArray);
         courseList.setForeground(Color.decode("#23272a"));
         courseList.setBackground(Color.decode("#99aab5"));
         ArrayList<classClass> courseArrayList = new ArrayList<classClass>();
         courseArrayList = currentUser.getclassList();
+        
+        
         String courses[] = new String[courseArrayList.size()];
 //       System.out.print("  108 = " + courseArrayList.get(0).className + "\n");
 //       System.out.print("  109 = " + courseArrayList.get(1).className + "\n");
 //       System.out.print("courseArrayList.getsize() = " +  courseArrayList.size() + "\n");
-        for (int increment = 0; increment < courseArrayList.size(); increment++)
-        {
-        	//System.out.print(courseArrayList.get(increment).className);
-        	courses[increment] = courseArrayList.get(increment).className;
+        for (int increment = 0; increment < courseArrayList.size(); increment++) {
+            //System.out.print(courseArrayList.get(increment).className);
+            courses[increment] = courseArrayList.get(increment).className;
 //        	System.out.print(increment + ") " +courses[increment] + "\n");
-        	
+
         }
         courseList.setListData(courses);
         courseList.setLayoutOrientation(JList.VERTICAL);
 
         scrollPane = new JScrollPane();			// Allows scrolling through list of tutors
-        scrollPane.setViewportView(courseList);	
+        scrollPane.setViewportView(courseList);
         scrollPane.setBounds(258, 375, 283, 150);
         panel.add(scrollPane);
-        
+
         titleLabel = new JLabel("Manage Classes");
         titleLabel.setBounds(0, 65, 800, 60);
         titleLabel.setFont(new Font(null, Font.CENTER_BASELINE, 40));
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
         titleLabel.setForeground(Color.decode("#dcddde"));
         panel.add(titleLabel);
-        
+
         scrollPaneLabel = new JLabel("Enrolled Courses To Tutor:");
         scrollPaneLabel.setForeground(Color.decode("#dcddde"));
         scrollPaneLabel.setFont(new Font(null, Font.PLAIN, 10));
         scrollPaneLabel.setBounds(258, 350, 200, 25);
         panel.add(scrollPaneLabel);
-      
-        
-        
+
         errorLabel = new JLabel("");
         errorLabel.setBounds(0, 300, 800, 35);
         errorLabel.setHorizontalAlignment(JLabel.CENTER);
@@ -163,6 +148,14 @@ public class ClassGui implements ActionListener {
         addButton.setFocusable(false);
         addButton.addActionListener(this);
         panel.add(addButton);
+
+        deleteBtn = new JButton("Delete Class");
+        deleteBtn.setBounds(550, 495, 125, 30);
+        deleteBtn.setBackground(Color.decode("#7289da"));
+        deleteBtn.setForeground(Color.decode("#dcddde"));
+        deleteBtn.setFocusable(false);
+        deleteBtn.addActionListener(this);
+        panel.add(deleteBtn);
 
         courseLabel = new JLabel("Enter course: ");
         courseLabel.setForeground(Color.decode("#dcddde"));
@@ -210,7 +203,7 @@ public class ClassGui implements ActionListener {
         frame.setTitle("Manage Classes");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    
+
     public void confirmPage(String text1) {
 
         confirmFrame = new JFrame();
@@ -235,6 +228,14 @@ public class ClassGui implements ActionListener {
         confirmBtn.addActionListener(this);
         confirmPanel.add(confirmBtn);
 
+        confirmBtn2 = new JButton("Confirm");
+        confirmBtn2.setBounds(110, 70, 80, 30);
+        confirmBtn2.setBackground(Color.decode("#7289da"));
+        confirmBtn2.setForeground(Color.decode("#dcddde"));
+        confirmBtn2.setFocusable(false);
+        confirmBtn2.addActionListener(this);
+        confirmPanel.add(confirmBtn2);
+
         declineBtn = new JButton("Decline");
         declineBtn.setBounds(210, 70, 80, 30);
         declineBtn.setBackground(Color.decode("#7289da"));
@@ -243,7 +244,7 @@ public class ClassGui implements ActionListener {
         declineBtn.addActionListener(this);
         confirmPanel.add(declineBtn);
 
-                ImageIcon image = new ImageIcon("src/main/GooberGLogo.png");
+        ImageIcon image = new ImageIcon("src/main/GooberGLogo.png");
         confirmFrame.setIconImage(image.getImage());
         confirmFrame.add(confirmPanel);
         confirmFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -269,7 +270,7 @@ public class ClassGui implements ActionListener {
         successLabel1.setForeground(Color.decode("#dcddde"));
         successPanel.add(successLabel1);
 
-                ImageIcon image = new ImageIcon("src/main/Frosty_POG-min.png");
+        ImageIcon image = new ImageIcon("src/main/GooberGLogo.png");
         successFrame.setIconImage(image.getImage());
         successFrame.add(successPanel);
         successFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -280,17 +281,7 @@ public class ClassGui implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == addButton) {
-            if (addText.getText().isEmpty()) {
-                errorLabel.setText("Please enter a course number");
-            } else if (gradeBox.getSelectedIndex() >= 2) {
-                errorLabel.setText("You do not have an appropriate grade to tutor this class.");
-            } else {
-            	String confirmText1 = "Do you want to add " + addText.getText().toUpperCase() + " as one of your classes to tutor?";
-            	confirmPage(confirmText1);
-                errorLabel.setText("");
-            }
-        }
+
         if (e.getSource() == homeButton) {
             HomePage home = new HomePage(currentUser);
             frame.dispose();
@@ -299,6 +290,36 @@ public class ClassGui implements ActionListener {
             LoginGui login = new LoginGui();
             frame.dispose();
         }
+        if (e.getSource() == addButton) {
+            if (addText.getText().isEmpty()) {
+                errorLabel.setText("Please enter a course number");
+            } else if (gradeBox.getSelectedIndex() >= 2) {
+                errorLabel.setText("You do not have an appropriate grade to tutor this class.");
+            } else {
+                String confirmText1 = "Do you want to add " + addText.getText().toUpperCase() + " as one of your classes to tutor?";
+                confirmPage(confirmText1);
+                confirmBtn.setVisible(true);
+                confirmBtn2.setVisible(false);
+                errorLabel.setText("");
+            }
+        }
+        
+        if (e.getSource() == deleteBtn){
+            
+            int index = courseList.getSelectedIndex();
+//            if (courseArrayList[index] == null){
+//                
+//                errorLabel.setText("Please select a course to delete");
+//            }else {
+            
+            String confirmText2 = "Do you want to delete (course selection) from your courses to tutor?";
+            confirmPage(confirmText2);
+            confirmBtn.setVisible(false);
+            confirmBtn2.setVisible(true);
+            errorLabel.setText("");
+//           }
+        }
+
         if (e.getSource() == confirmBtn) {
 
             String successText1 = "You have successfully add " + addText.getText().toUpperCase();
@@ -308,6 +329,16 @@ public class ClassGui implements ActionListener {
             addText.setText("");
             gradeBox.setSelectedIndex(0);
         }
+
+        if (e.getSource() == confirmBtn2) {
+
+            String successText2 = "You have successfully deleted (course) from your courses to tutor.";
+            successPage(successText2);
+            confirmFrame.dispose();
+            addText.setText("");
+            gradeBox.setSelectedIndex(0);
+        }
+
         if (e.getSource() == declineBtn) {
             confirmFrame.dispose();
         }
